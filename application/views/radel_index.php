ea69f358bcad0a6e31013ed63dbe24127eceda21
mathias decker <mathias.decker@mdc-berlin.de>
2016-07-21 11:58:26 +0200

    added intro text

--- application/views/radel_index.php
+++ application/views/radel_index.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER['SERVER_NAME']=="wram-test.mdc-berlin.net") {
-    echo "<h4 style='background: red; border: 1px solid black; border-radius: 5px; padding 10px;'>dies ist das WRAM-Testsytem</h4>";
+    echo "<h4 style='background: red; border: 1px solid black; border-radius: 5px; padding 10px; margin: 10px;'>dies ist das WRAM-Testsytem</h4>";
 
 }
 if($_SERVER['SERVER_NAME']=="wram.mdc-berlin.net") {

273c9b184f476be5bbc98490074b180b5d08a10b
mathias decker <mathias.decker@mdc-berlin.de>
2016-07-27 14:16:16 +0200

    modyfied greeting text

--- application/views/radel_index.php
+++ application/views/radel_index.php
@@ -1,10 +1,5 @@
 <?php
-if($_SERVER['SERVER_NAME']=="wram-test.mdc-berlin.net") {
-    echo "<h4 style='background: red; border: 1px solid black; border-radius: 5px; padding 10px; margin: 10px;'>dies ist das WRAM-Testsytem</h4>";
-
-}
-if($_SERVER['SERVER_NAME']=="wram.mdc-berlin.net") {
-$text[en] = "Dear colleagues,
+$text['en'] = "Dear colleagues,
 
 Get ready to mount the saddle ­ of your bike. Aug. 1 is the first day of a cycling competition in Berlin, which pits participants from the Buch campus against other institutes and companies throughout the city.
   
@@ -31,7 +26,7 @@ Get with it and stay fit!
 
 Your Green Ambassadors";
 
-$text[de] = "Liebe Kolleginnnen und Kollegen,
+$text['de'] = "Liebe Kolleginnnen und Kollegen,
   
 am 1. August geht es los: Wir vom Campus Buch treten gegen andere öffentliche Betriebe Berlins in einem Fahrrad-Wettbewerb an. Bei \"Wer radelt am meisten?\" geht es um die Wege, die mit dem Rad von den Teilnehmerinnen und Teilnehmern gefahren werden. Die Kilometer werden zusammengerechnet und mit den anderen teilnehmenden Unternehmen
 verglichen. Es zählen alle Strecken ­ es ist also egal, ob sie auf dem Weg zum Campus oder in der Freizeit gefahren werden. Die Aktion läuft bis Ende September; der Einstieg ist jederzeit möglich.
@@ -52,7 +47,23 @@ www.wer-radelt-am-meisten.de
 Spezifische Fragen beantworten
 green.campus@mdc-berlin.de";
 
-    echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+$text['de-de'] = $text['de'];
+
+$lang = array_shift(explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']));
+$body = nl2br($text[$lang]);
+if($body == '') {
+    $body = nl2br($text['en']);
+}
+
+
+
+if($_SERVER['SERVER_NAME']=="wram-test.mdc-berlin.net") {
+    echo "<h4 style='background: red; border: 1px solid black; border-radius: 5px; padding 10px; margin: 10px;'>dies ist das WRAM-Testsytem</h4>";
+
+}
+if($_SERVER['SERVER_NAME']=="wram.mdc-berlin.net") {
+
+    echo $body;
 }
 
 ?>

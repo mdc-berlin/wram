58e4e630103d01c89a424b3bac94ea350a8a2a58
mathias decker <mathias.decker@mdc-berlin.de>
2016-07-28 21:27:43 +0200

    ministats

--- application/views/radel_index.php
+++ application/views/radel_index.php
@@ -23,10 +23,10 @@ if($_SERVER['SERVER_NAME']=="wram.mdc-berlin.net") {
         <?= $strings['text'][$lang]; ?>
     </div>
     <div style="float: left; background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">
-        <table style="padding: 2px; margin: 2px;">
+        <table>
             <tr style=" border: 1px solid gray">
-                <td><b><?= $strings['department'][$lang]; ?></b></td>
-                <td><b><?= $strings['user'][$lang]; ?></b></td>
+                <td style="padding: 2px; margin: 2px;"><b><?= $strings['department'][$lang]; ?></b></td>
+                <td style="padding: 2px; margin: 2px;"><b><?= $strings['user'][$lang]; ?></b></td>
             </tr>
             <?php
             $query = $this->db->query("select count(*) as c, substring_index(Abteilung,'/',1) as d from teilnehmer where Abteilung != '' group by d order by c");

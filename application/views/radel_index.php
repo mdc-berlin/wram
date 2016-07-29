153d114dccc80bbe954b49a69a0dd269dde53ab1
mathias decker <mathias.decker@mdc-berlin.de>
2016-07-29 15:42:19 +0200

    bugfix

--- application/views/radel_index.php
+++ application/views/radel_index.php
@@ -21,28 +21,38 @@ if($_SERVER['SERVER_NAME']=="wram.mdc-berlin.net") {
 <div style="">
 
 
-    <div style="float: right; width: 25%; background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">
-        <table>
-            <tr style=" border-bottom: 1px solid gray">
-                <td style="padding: 2px; margin: 2px;"><b>MDC <?= $strings['department'][$lang]; ?></b></td>
-                <td style="padding: 2px; margin: 2px;"><b><?= $strings['user'][$lang]; ?></b></td>
-            </tr>
-            <?php
-            $query = $this->db->query("select count(*) as c, substring_index(Abteilung,'/',1) as d from teilnehmer where Abteilung != '' group by d order by c DESC ");
-            foreach($query->result() as $row) {
-                ?><tr>
-                    <td>
-                        &nbsp;<?= $row->d; ?>
-                    </td>
-                    <td style="text-align: right">
-                        <?= $row->c; ?>&nbsp;
-                    </td>
+    <div style="float: right; width: 25%;">
+        <div style=" background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">
+            <table>
+                <tr style=" border-bottom: 1px solid gray">
+                    <td style="padding: 2px; margin: 2px;"><b>WRAM-Team</b></td>
+                    <td style="padding: 2px; margin: 2px;"><b><?= $strings['user'][$lang]; ?></b></td>
                 </tr>
-            <?php
-            }
-            ?>
-            <tr><td colspan="2" style="text-align: right"><small><?= $strings['department_desc'][$lang]; ?></small></td></tr>
-        </table>
+            </table>
+        </div>
+        <div style=" background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">
+            <table>
+                <tr style=" border-bottom: 1px solid gray">
+                    <td style="padding: 2px; margin: 2px;"><b>MDC <?= $strings['department'][$lang]; ?></b></td>
+                    <td style="padding: 2px; margin: 2px;"><b><?= $strings['user'][$lang]; ?></b></td>
+                </tr>
+                <?php
+                $query = $this->db->query("select count(*) as c, substring_index(Abteilung,'/',1) as d from teilnehmer where Abteilung != '' group by d order by c DESC ");
+                foreach($query->result() as $row) {
+                    ?><tr>
+                        <td>
+                            &nbsp;<?= $row->d; ?>
+                        </td>
+                        <td style="text-align: right">
+                            <?= $row->c; ?>&nbsp;
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+                <tr><td colspan="2" style="text-align: right"><small><?= $strings['department_desc'][$lang]; ?></small></td></tr>
+            </table>
+        </div>
     </div>
     <div style="float: right; width: 70%; background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">
         <?= $strings['text'][$lang]; ?>

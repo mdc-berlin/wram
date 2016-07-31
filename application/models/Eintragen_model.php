f2c9c42087be6d8c5a43a581c80202f02d2acb08
Mathias Decker <mathias@itgweb.de>
2016-07-31 15:25:16 +0200

    added department stat

--- application/models/Eintragen_model.php
+++ application/models/Eintragen_model.php
@@ -730,7 +730,7 @@ class Eintragen_model extends CI_Model {
 						FROM teilnehmer t JOIN fahrtenbuch f ON (t.id = f.`Teilnehmer_id`) WHERE Team_id IN($group_id) GROUP BY Datum");
 				} else {
 					$query = $this->db->query("SELECT Datum, SUM(f.`Km_zur_Arbeit`) as Km_Arbeit_sum, SUM(f.`Km_Privat`) as Km_Privat_sum
-						FROM teilnehmer t JOIN fahrtenbuch f ON (t.id = f.`Teilnehmer_id`) WHERE Abteilung = '$group_id' GROUP BY Datum");
+						FROM teilnehmer t JOIN fahrtenbuch f ON (t.id = f.`Teilnehmer_id`) WHERE Abteilung = $group_id GROUP BY Datum");
 				}
 				if ($query->num_rows() > 0) {
 					foreach ($query->result() as $row) {

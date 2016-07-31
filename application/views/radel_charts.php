a58da7578e4384def30299f774cd0bff305f450e
Mathias Decker <mathias@itgweb.de>
2016-07-31 15:33:00 +0200

    added department stat

--- application/views/radel_charts.php
+++ application/views/radel_charts.php
@@ -1,7 +1,9 @@
 <?php
 require('translations.php');
+print_r($_SERVER);
 ?>
-		<h2><?= $strings['stats'][$lang]; ?></h2>
+		<h2><?= $strings['stats'][$lang]; ?>: </h2>
+
 			<div>
 				<canvas id="canvas"></canvas>
 			</div>

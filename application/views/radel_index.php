ab23f10be6159b0fcfcd15064c175417bd840205
mathias decker <mathias.decker@mdc-berlin.de>
2016-08-18 13:00:12 +0200

    overview

--- application/views/radel_index.php
+++ application/views/radel_index.php
@@ -79,7 +79,7 @@ if($_SERVER['SERVER_NAME']=="wram.mdc-berlin.net") {
     <div style="float: right; width: 70%; ">
         <div style=" background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">
             <div style="border: 1px solid black; border-radius: 3px; width: 100%; ">
-                <div style="background: black; width: 50%; margin: 1px; height: 2px"></div>
+                <div style="background: #2b2b2b; width: 50%; margin: 1px; height: 4px"></div>
             </div>
         </div>
         <div style=" background-color: rgba(255,255,255,0.75); border-radius: 5px; padding: 10px; margin: 10px">

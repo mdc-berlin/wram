3de9d39bd1c9644ed12f61e7a4472291ff2d2778
mathias decker <mathias.decker@mdc-berlin.de>
2017-06-20 13:28:30 +0200

    bugfix

--- application/views/radel_index.php
+++ application/views/radel_index.php
@@ -47,7 +47,7 @@ print_r($this);
                     $userobject =  $this->db->query("select * from `teilnehmer` where concat(`Vorname`,' ',`Name`) = '".$user."'");
                     $userid = $userobject->result();
                     //print_r($userid[0]);
-                    if($userid) {
+                    if($userid && $userid[0]->id) {
                         $result = $this->db->query("SELECT Vorname, Name, (sum(fahrtenbuch.Km_zur_Arbeit)+sum(fahrtenbuch.Km_Privat)) as km FROM fahrtenbuch inner join teilnehmer on fahrtenbuch.Teilnehmer_id = teilnehmer.id
                         where year(datum) = 2017
                         and Teilnehmer_id = ".$userid[0]->id." group by Teilnehmer_id order by km;");
@@ -68,7 +68,7 @@ print_r($this);
                     <td> <?= $strings['department'][$lang]; ?></td>
                     <td style="text-align: right"><?php
                     if($userid) {
-                        echo $userid[0]->Abteilung; 
+                        echo $userid[0]->Abteilung;
                     }
                     ?></td>
                 </tr>
